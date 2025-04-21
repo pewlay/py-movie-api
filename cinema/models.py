7b6ee31ec17e18606a17bb9fe1681e9ff1497986
Polina Haltseva <polinayut@gmail.com>
2025-04-21 23:48:08 +0300

    Add necessary files

---
+++ cinema/models.py
@@ -0,0 +1,14 @@
+from django.db import models
+
+
+class Movie(models.Model):
+    title = models.CharField(max_length=255)
+    description = models.TextField()
+    duration = models.PositiveIntegerField()
+
+    class Meta:
+        ordering = ('title', )
+        verbose_name_plural = 'movies'
+
+    def __str__(self):
+        return f'{self.title} duration: {self.duration}'
